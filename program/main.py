561d98c2e56c96600021be9bbea2831d1c3cc00e
Joshua Chen <joshuachen.04@gmail.com>
2021-09-25 11:36:58 -0500

    add function to read in air quality data

--- program/main.py
+++ program/main.py
@@ -1,5 +1,26 @@
 def main():
+    # print("Team biocode")
+    columnLables, cityDictionary = readDataFromFile()
+    print(columnLables)
+    print(cityDictionary)
+    pass
 
+def readDataFromFile():
+    airQualityFileName = "AirQualityDataPoints.txt"
 
-if __name__ == "__main__":
-    main()
+    columnLables = []
+    cityDictionary = {}
+
+    with open(airQualityFileName, "r") as file:
+        # read column labels
+        columnLables = file.readline().split(",")
+
+        # read and parse in data
+        dataLines = file.readlines().split("/n")
+        for line in dataLines:
+            cityName, dataPoint = file.readline().split(",")
+            dataPoint = map(int, dataPoint)
+
+            cityDictionary[cityName] = dataPoint
+
+    return columnLables, cityDictionary
